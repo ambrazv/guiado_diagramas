5f8adf1d120de845ed65f54f8c8fab6c1a17a23e
Ambar Zambrano Venegas <ambaar.venegas@gmail.com>
2024-07-24 20:31:50 -0400

    agregando

---
+++ grupo.py
@@ -0,0 +1,26 @@
+# Diagrama de flujo
+# Sumar dos números enteros ingresados por el usuario, mostrando el resultado de la suma.
+
+#Paso 1
+
+# Realizar una tabla para desglosar el problema en tres partes:
+
+# 1ro = Datos de Entrada
+# 2do = Proceso
+# 3ro = Datos de Salida 
+
+import math
+
+# números ingresados por el usuario
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+#operación 
+
+Suma = numero1 + numero2
+
+#Resultado de la operación 
+
+print ("La suma de los dos números es:", Suma)
+
+#Fin del ejercicio
